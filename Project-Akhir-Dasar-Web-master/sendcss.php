eb77cff3a835608b5052f9bbc23da8cd619a2ad7
wahidari <wahiid.ari@gmail.com>
2020-06-06 04:33:43 +0700

    publish

--- Project-Akhir-Dasar-Web-master/sendcss.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = file_get_contents('php://input');
-$inp = file_get_contents('datacss.txt');
-$tempArray = json_decode($inp);
-array_push($tempArray, json_decode($data));
-$jsonData = json_encode($tempArray);
-file_put_contents('datacss.txt', $jsonData);
-?>
